ed60cb4929576bae9fee9086aa787e9ebf9cbf01
Ovidio R. Calvet <calveto@gmail.com>
2019-01-01 14:57:20 -0500

    Move scrambling logic into its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:rubik_timer/scrambler.dart';
 import 'package:vibrate/vibrate.dart';
 import 'dart:async';
 
-const List<String> MOVE_TYPES = ["R", "L", "U", "D", "B", "F"];
-const List<String> MOVE_TYPES_OPTIONS = ["", "'", "2"];
 const COUNTDOWN_OPTIONS = <String>['5', '10', '15', '20', '25', '30'];
 
 void main() {
@@ -14,23 +11,6 @@ void main() {
     DeviceOrientation.portraitUp
   ]);
   runApp(MyApp());
-}
-
-List<String> scrambleRubikMoves(int totalMoves) {
-  List<String> result = List<String>();
-  int randTypeIdx = 0;
-  int randTypeOptionIdx = 0;
-  String move;
-  Random rnd = Random();
-  for (int i = 0; i < totalMoves; i++) {
-    do {
-      randTypeIdx = rnd.nextInt(MOVE_TYPES.length);
-      randTypeOptionIdx = rnd.nextInt(MOVE_TYPES_OPTIONS.length);
-      move = MOVE_TYPES[randTypeIdx] + MOVE_TYPES_OPTIONS[randTypeOptionIdx];
-    } while (i > 0 && move[0].compareTo(result[i - 1][0]) == 0);
-    result.add(move);
-  }
-  return result;
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/scrambler.dart
@@ -0,0 +1,22 @@
+
+import 'dart:math';
+
+const List<String> MOVE_TYPES = ["R", "L", "U", "D", "B", "F"];
+const List<String> MOVE_TYPES_OPTIONS = ["", "'", "2"];
+
+List<String> scrambleRubikMoves(int totalMoves) {
+  List<String> result = List<String>();
+  int randTypeIdx = 0;
+  int randTypeOptionIdx = 0;
+  String move;
+  Random rnd = Random();
+  for (int i = 0; i < totalMoves; i++) {
+    do {
+      randTypeIdx = rnd.nextInt(MOVE_TYPES.length);
+      randTypeOptionIdx = rnd.nextInt(MOVE_TYPES_OPTIONS.length);
+      move = MOVE_TYPES[randTypeIdx] + MOVE_TYPES_OPTIONS[randTypeOptionIdx];
+    } while (i > 0 && move[0].compareTo(result[i - 1][0]) == 0);
+    result.add(move);
+  }
+  return result;
+}
