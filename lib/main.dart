aa204a80bb8f7d3fa583e691993722c1ab735ccf
Ovidio R. Calvet <calveto@gmail.com>
2018-12-31 22:52:03 -0500

    Generate 25 random moves to scramble the cube

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,43 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:vibrate/vibrate.dart';
 import 'dart:async';
 
-void main() => runApp(MyApp());
+const List<String> MOVE_TYPES = ["R", "L", "U", "D", "B", "F"];
+const List<String> MOVE_TYPES_OPTIONS = ["", "'", "2"];
+
+void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
+  ]);
+  runApp(MyApp());
+}
+
+List<String> scrambleRubikMoves(int totalMoves) {
+  List<String> result = List<String>();
+  int randTypeIdx = 0;
+  int randTypeOptionIdx = 0;
+  String move;
+  Random rnd = Random();
+  for (int i = 0; i < totalMoves; i++) {
+    do {
+      randTypeIdx = rnd.nextInt(MOVE_TYPES.length);
+      randTypeOptionIdx = rnd.nextInt(MOVE_TYPES_OPTIONS.length);
+      move = MOVE_TYPES[randTypeIdx] + MOVE_TYPES_OPTIONS[randTypeOptionIdx];
+    } while (i > 0 && move[0].compareTo(result[i - 1][0]) == 0);
+    result.add(move);
+  }
+  return result;
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Rubik Crazy Timer',
+      title: 'Rubik Pro Timer',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: MyHomePage(title: 'Rubik Pro Timer'),
@@ -32,6 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Color textColor = Colors.white70;
   Color backgroundColor = Colors.black38;
   String countDownSeconds = '15';
+  List<String> scrambledMoves = scrambleRubikMoves(25);
   // Stopwatch sw;
   @override
   void initState() {
@@ -53,10 +82,11 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
         actions: <Widget>[
           DropdownButton<String>(
-            items: <String>['5', '10', '15', '20', '25', '30'].map((String value) {
-              return new DropdownMenuItem<String>(
+            items:
+                <String>['5', '10', '15', '20', '25', '30'].map((String value) {
+              return DropdownMenuItem<String>(
                 value: value,
-                child: new Text(value),
+                child: Text(value),
               );
             }).toList(),
             value: countDownSeconds,
@@ -86,6 +116,32 @@ class _MyHomePageState extends State<MyHomePage> {
           height: double.infinity,
           child: Stack(
             children: <Widget>[
+              Positioned.fill(
+                top: -350,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        scrambledMoves
+                            .getRange(0, 12)
+                            .reduce((a, b) => a + "  " + b),
+                        style: TextStyle(fontSize: 19),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        scrambledMoves
+                            .getRange(13, scrambledMoves.length)
+                            .reduce((a, b) => a + "  " + b),
+                        style: TextStyle(fontSize: 19),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
               Positioned(
                 left: 20,
                 bottom: 30,
@@ -132,7 +188,7 @@ class _MyHomePageState extends State<MyHomePage> {
     countDownTimer = Timer.periodic(Duration(seconds: 1), (t) {
       setState(() {
         countDown--;
-        if (countDown == 8) {
+        if (countDown <= 8) {
           textColor = Colors.black;
           backgroundColor = Colors.redAccent[700];
         }
@@ -158,6 +214,7 @@ class _MyHomePageState extends State<MyHomePage> {
       countDown = int.parse(countDownSeconds);
       textColor = Colors.white70;
       backgroundColor = Colors.black38;
+      scrambledMoves = scrambleRubikMoves(25);
       timer.cancel();
       countDownTimer.cancel();
     });
