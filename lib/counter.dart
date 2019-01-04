db24cda5dc00d80f7e7db883180d73d4e909bc1d
Ovidio Calvet <calveto@gmail.com>
2019-01-03 23:46:32 -0500

    Add minutes to the display

--- lib/counter.dart
+++ lib/counter.dart
@@ -5,11 +5,13 @@ class Counter extends StatelessWidget {
   Counter({Key key, this.total100milliseconds}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    int seconds = (total100milliseconds / 10).floor();
+    int totalSeconds = (total100milliseconds / 10).floor();
+    int minutes = (totalSeconds / 60).floor();
+    int seconds = totalSeconds % 60;
     int ms = total100milliseconds % 10;
     return Center(
       child: Text(
-        '$seconds.$ms',
+        '$minutes:$seconds.$ms',
         style: Theme.of(context).textTheme.display1.copyWith(
               fontSize: 110,
               fontFamily: 'Orbitron',
